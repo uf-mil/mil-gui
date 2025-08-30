91349cddf00089e9fac0c875d64cddbffcb4a36b
Joseph Handsome <nottellingu@gmail.com>
2025-08-29 23:26:28 -0400

    better reconnect logic with useEffect
    changes the reconnect to useEffect + interval instead of setTimeout hell

--- src/components/RosContext.tsx
+++ src/components/RosContext.tsx
@@ -24,44 +24,20 @@ export function RosProvider({ children }: RosProviderProps) {
     rosRef.current.on('connection', function() {
         console.log('Connected to ROS!');
         setConnected(true);
-        clear_timer()
     });
 
     rosRef.current.on('error', function(error) {
         console.log('ROS connection error:', error);
         setConnected(false);
-        set_timer() // for reconnect
     });
 
     rosRef.current.on('close', function() {
         console.log('ROS connection closed');
         setConnected(false);
-        set_timer() // for reconnect
     });
 
-    const reconnectTimerRef = useRef<NodeJS.Timeout | null>(null);
-    const reconnectInterval = 1000; // in ms
 
-    // set timer to null
-    function clear_timer(){
-        if (reconnectTimerRef.current) {
-            clearTimeout(reconnectTimerRef.current);
-            reconnectTimerRef.current = null;
-            console.log("Reconnect timer cleared");
-        }
-    }
-
-    // sets a timeout before attempting to connect again
-    function set_timer(){
-        // if timer already set, do nothing
-        if (!(reconnectTimerRef.current === null)) { return }
-
-        console.log(`Scheduling reconnect attempt in ${reconnectInterval}ms`);
-        reconnectTimerRef.current = setTimeout(connect_to_ros, reconnectInterval)
-    }
-
-    function connect_to_ros(){
-        clear_timer()
+    function connect_to_ros() {
         rosRef.current.connect('ws://localhost:9090');
     }
 
@@ -73,12 +49,36 @@ export function RosProvider({ children }: RosProviderProps) {
 
         // Cleanup on unmount
         return () => {
-            clear_timer()
             if (rosRef.current.isConnected) {
                 rosRef.current.close();
             }
         };
     }, []);
+
+    // on connect or disconnect
+    const reconnectTimerRef = useRef<NodeJS.Timer | null>(null);
+    const reconnectInterval = 5000; // in ms
+    useEffect(() => {
+        // if connected, stop trying to connect and return
+        const connected_but_still_trying_to_reconnect = (connected == true) && !(reconnectTimerRef.current === null)
+        if (connected_but_still_trying_to_reconnect) {
+            clearInterval(reconnectTimerRef.current!)
+            reconnectTimerRef.current = null
+            return
+        }
+
+        // if not connected and not trying to reconnect, start trying
+        const not_connected_and_not_trying_to_reconnect = (connected == false) && (reconnectTimerRef.current === null)
+        if (not_connected_and_not_trying_to_reconnect) {
+            reconnectTimerRef.current = setInterval(connect_to_ros, reconnectInterval)
+        }
+
+        // Cleanup on unmount
+        return () => {
+            clearInterval(reconnectTimerRef.current!)
+            reconnectTimerRef.current = null
+        };
+    }, [connected])
 
     return (
         <RosContext.Provider value={{ ros: rosRef.current, connected }}>
